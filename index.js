bfd75eb45f8ff35ec3049d373c53c16d2cf90065
SteveMustanski <steve.mustanski@gmail.com>
2023-11-28 12:47:07 -0500

    added using superagent promise

--- index.js
+++ index.js
@@ -3,18 +3,16 @@ const superagent = require('superagent');
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}` );
-  superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) {
-        return console.log(err.message);
-      } else {
-        console.log(res.body.message);
-      }
-      
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then( res => {
+      console.log(res.body.message);
       fs.writeFile('dog-img.txt', res.body.message, err => {
         if(err) {
           return console.log(err.message);
         } else console.log('Random image saved.');
-      })
+      });
+  }).catch(err => {
+    console.log(err.message);
   });
 });
